0c7315b1de151f3013de029da025300a0d36cd74
gb23 <gregbenj@hotmail.com>
2017-09-20 10:54:14 -0700

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,15 @@
 Rails.application.routes.draw do
-  devise_for :users
+    devise_for :users
+   #resources :users, only: [:show]
   root 'store#index', as: 'store'
 
   resources :items, only: [:show, :index]
   resources :categories, only: [:show, :index]
-  #resources :users, only: [:show]
+ 
   resources :carts
   resources :line_items, only: [:create]
   resources :orders, only: [:show]
 
   post 'carts/:id/checkout', to: 'carts#checkout', as: 'checkout'
-  get '/users/sign_out', to: 'store#index'
+
 end
